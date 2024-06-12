82cfb86724de5861cb034bd451fad3884eae8b77
khyaejin <olivia019182@naver.com>
2024-06-12 20:52:18 +0900

    ✨ 상품 사진 수정

--- src/main/java/dao/ProductRepository.java
+++ src/main/java/dao/ProductRepository.java
@@ -20,23 +20,25 @@ public class ProductRepository {
 		product1.setCategory("전자제품");
 		product1.setReleaseDate("2022/11/10");
 		product1.setCondition("중고상품");
-		product1.setFilename("SKU1234.jpg");
+		product1.setFilename("iphone.jpg");
 		
-		Product product2 = new Product("SKU1235", "삼성 갤럭시 S21", 700000);
+		Product product2 = new Product("SKU1235", "캐주얼 후드 스웨트셔츠", 70000);
 		product2.setRegistrant("한민지");
-		product2.setDescription("삼성 갤럭시 S21 중고폰입니다. 화면에 약간의 스크래치가 있지만, 사용에는 문제가 없습니다.");
-		product2.setCategory("전자제품");
+		product2.setDescription("스타일리시한 캐주얼 후드 스웨트셔츠입니다. 앞면에 로고가 프린팅되어 있으며, 여러 색상과 사이즈로 제공됩니다. 사진은 모델 사진 첨부합니다.");
+		product2.setCategory("의류");
 		product2.setReleaseDate("2023/01/05");
-		product2.setCondition("중고상품");
-		product2.setFilename("SKU1235.jpg");
+		product2.setCondition("새 제품");
+		product2.setFilename("hoodie.jpg");
+
 		
-		Product product3 = new Product("SKU1236", "LG 그램 노트북", 900000);
+		Product product3 = new Product("SKU1236", "플로랄 무드 향수", 90000);
 		product3.setRegistrant("이민수");
-		product3.setDescription("LG 그램 2021년형 모델입니다. 가벼운 사용감이 있으며, 모든 기능 정상 작동합니다.");
-		product3.setCategory("전자제품");
+		product3.setDescription("매혹적인 플로럴 무드 향수입니다. 부드러운 향이 지속되며, 섬세하고 여성스러운 느낌을 전달합니다.");
+		product3.setCategory("향수");
 		product3.setReleaseDate("2023/02/15");
-		product3.setCondition("중고상품");
-		product3.setFilename("SKU1236.jpg");
+		product3.setCondition("새 제품");
+		product3.setFilename("perfume.jpg");
+
 		
 		listOfProducts.add(product1);
 		listOfProducts.add(product2);
