347d2e78129cdd9023060d6a01d1d66ae1bb9467
khyaejin <olivia019182@naver.com>
2024-06-12 20:25:04 +0900

    ✨ 상품 정보 수정

--- src/main/java/dao/ProductRepository.java
+++ src/main/java/dao/ProductRepository.java
@@ -14,29 +14,29 @@ public class ProductRepository {
 	}
 	
 	public ProductRepository() {
-		Product product1 = new Product("ISBN1234", "C# 프로그래밍", 27000);
-		product1.setRegistrant("우재남");
-		product1.setDescription("C#을 처음 접하는 독자를 대상으로 일대일 수업처럼 자세히 설명한 책이다. 꼭 알아야 할 핵심 개념은 기본 예제로 최대한 쉽게 설명했으며, 중요한 내용은 응용 예제, 퀴즈, 셀프 스터디, 예제 모음으로 한번 더 복습할 수 있다.");
-		product1.setCategory("IT모바일");
-		product1.setReleaseDate("2022/10/06");
-		product1.setCondition("신상품");
-		product1.setFilename("ISBN1234.jpg");
+		Product product1 = new Product("SKU1234", "아이폰 12", 600000);
+		product1.setRegistrant("김혜진");
+		product1.setDescription("깨끗하게 사용한 아이폰 12입니다. 기스 거의 없고, 배터리 성능도 양호합니다.");
+		product1.setCategory("전자제품");
+		product1.setReleaseDate("2022/11/10");
+		product1.setCondition("중고상품");
+		product1.setFilename("SKU1234.jpg");
 		
-		Product product2 = new Product("ISBN1235", "자바마스터", 30000);
-		product2.setRegistrant("송미영");
-		product2.setDescription("자바를 처음 배우는 학생을 위해 자바의 기본 개념과 실습 예제를 그림을 이용하여 쉽게 설명합니다. 자바의 이론적 개념→기본 예제→프로젝트 순으로 단계별 학습이 가능하며, 각 챕터의 프로젝트를 실습하면서 온라인 서점을 완성할 수 있도록 구성하였습니다.");
-		product2.setCategory("IT모바일");
-		product2.setReleaseDate("2023/01/01");
-		product2.setCondition("신상품");
-		product2.setFilename("ISBN1235.jpg");
+		Product product2 = new Product("SKU1235", "삼성 갤럭시 S21", 700000);
+		product2.setRegistrant("한민지");
+		product2.setDescription("삼성 갤럭시 S21 중고폰입니다. 화면에 약간의 스크래치가 있지만, 사용에는 문제가 없습니다.");
+		product2.setCategory("전자제품");
+		product2.setReleaseDate("2023/01/05");
+		product2.setCondition("중고상품");
+		product2.setFilename("SKU1235.jpg");
 		
-		Product product3 = new Product("ISBN1236", "파이썬 프로그래밍", 30000);
-		product3.setRegistrant("최성철");
-		product3.setDescription("파이썬으로 프로그래밍을 시작하는 입문자가 쉽게 이해할 수 있도록 기본 개념을 상세하게 설명하며, 다양한 예제를 제시합니다. 또한 프로그래밍의 기초 원리를 이해하면서 파이썬으로 데이터를 처리하는 기법도 배웁니다.");
-		product3.setCategory("IT모바일");
-		product3.setReleaseDate("2023/01/01");
-		product3.setCondition("신상품");
-		product3.setFilename("ISBN1236.jpg");
+		Product product3 = new Product("SKU1236", "LG 그램 노트북", 900000);
+		product3.setRegistrant("이민수");
+		product3.setDescription("LG 그램 2021년형 모델입니다. 가벼운 사용감이 있으며, 모든 기능 정상 작동합니다.");
+		product3.setCategory("전자제품");
+		product3.setReleaseDate("2023/02/15");
+		product3.setCondition("중고상품");
+		product3.setFilename("SKU1236.jpg");
 		
 		listOfProducts.add(product1);
 		listOfProducts.add(product2);
